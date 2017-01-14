81f16f5441279ca5a3f3b51f43f9cf1b8e143758
Vladislav Titov <titovvlad1997@yandex.ru>
2017-01-14 21:05:18 +0300

    test branches

--- app/src/main/java/com/example/vladislav/nasaapi/MainActivity.java
+++ app/src/main/java/com/example/vladislav/nasaapi/MainActivity.java
@@ -11,6 +11,10 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+/**
+ * Polina branch: test branches!
+ */
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     Toolbar toolbar;

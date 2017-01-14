d228cf6ac9dceaa9757f10bbeb4db41e4e73d755
Vladislav Titov <titovvlad1997@yandex.ru>
2017-01-14 20:05:00 +0300

    added navigation view

--- app/src/main/java/com/example/vladislav/nasaapi/MainActivity.java
+++ app/src/main/java/com/example/vladislav/nasaapi/MainActivity.java
@@ -1,13 +1,69 @@
 package com.example.vladislav.nasaapi;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    Toolbar toolbar;
+    DrawerLayout drawer;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        );
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.nav_daily:
+                Snackbar.make(drawer, "It's daily photo!", Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_mars:
+                Snackbar.make(drawer, "It's mars rovers photos!", Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Snackbar.make(drawer, "It's settings!", Snackbar.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_exit:
+                finish();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }

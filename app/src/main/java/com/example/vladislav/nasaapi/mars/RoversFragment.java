85ef15db63b01ba77132238313a7059fe2660c5f
Vladislav Titov <titovvlad1997@yandex.ru>
2017-01-15 19:45:48 +0300

    added mars rovers

---
+++ app/src/main/java/com/example/vladislav/nasaapi/mars/RoversFragment.java
@@ -0,0 +1,10 @@
+package com.example.vladislav.nasaapi.mars;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Vladislav on 15.01.2017.
+ */
+
+public class RoversFragment extends Fragment {
+}

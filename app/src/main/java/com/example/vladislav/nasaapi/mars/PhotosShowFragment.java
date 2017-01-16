5706c80085b5788bf3120ffa8d50ff882d94ab00
Vladislav Titov <titovvlad1997@yandex.ru>
2017-01-16 03:22:53 +0300

    create photos show fragment

---
+++ app/src/main/java/com/example/vladislav/nasaapi/mars/PhotosShowFragment.java
@@ -0,0 +1,10 @@
+package com.example.vladislav.nasaapi.mars;
+
+import android.app.Fragment;
+
+/**
+ * Created by Vladislav on 16.01.2017.
+ */
+
+public class PhotosShowFragment extends Fragment {
+}
